a78cc02e93f918c9aed7d473198f7219f8257add
Rush <haidar.abugurain@gmail.com>
2023-09-13 10:45:48 +0300

    store bookmarks in user's document and load them upon entering the bookmarks screen

--- lib/providers/bookmarks_provider.dart
+++ lib/providers/bookmarks_provider.dart
@@ -1,20 +1,53 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:speciality_coffee_review/models/review.dart';
+import 'package:speciality_coffee_review/models/review_database.dart';
+import 'package:speciality_coffee_review/providers/reviews_provider.dart';
 
 class BookmarksNotifier extends StateNotifier<List<Review>> {
-  BookmarksNotifier() : super([]);
+  final Future<List<dynamic>> bookmarks = ReviewDatabase.getBookmarkedReviews();
+  final List<Review> reviews;
+  BookmarksNotifier(this.reviews) : super([]);
 
-  void toggleBookmark(Review review) {
-    final isBookmarked = state.any((element) => element.id == review.id);
+  Future<void> loadBookmarks() async {
+    final bookmarkedReviews = await bookmarks;
+    final bookmark = [];
+    for (var review in reviews) {
+      if (bookmarkedReviews.contains(review.getId())) {
+        bookmark.add(review);
+      }
+    }
+    state = [...bookmark];
+  }
+
+  void toggleBookmark(Review review) async {
+    final bookmarkedReviews = await bookmarks;
+    final isBookmarked = bookmarkedReviews
+        .any((element) => element.toString() == review.getId());
 
     if (isBookmarked) {
-      state = state.where((r) => r.id != review.id).toList();
+      ReviewDatabase.removeBookmark(review);
+      bookmarkedReviews.remove(review.getId());
     } else {
-      state = [...state, review];
+      ReviewDatabase.bookmarkReview(review);
+      bookmarkedReviews.add(review.getId());
+    }
+
+    for (var review in reviews) {
+      if (bookmarkedReviews.contains(review.getId())) {
+        state = [...state, review];
+      } else {
+        state = [
+          ...state.where((element) => element.getId() != review.getId())
+        ];
+      }
     }
   }
 }
 
 final bookmarksProvider =
-    StateNotifierProvider<BookmarksNotifier, List<Review>>(
-        (ref) => BookmarksNotifier());
+    StateNotifierProvider<BookmarksNotifier, List<Review>>((ref) {
+  final list = ref.read(reviewsProvider);
+  late List<Review> reviews;
+  list.whenData((value) => reviews = value);
+  return BookmarksNotifier(reviews);
+});
